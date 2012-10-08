b458448512ec4cba5906bd2c442a31b8a738c385
Thomas E. Enebo <tom.enebo@gmail.com>
2012-10-08 18:44:25 -0500

    Remove comments

--- samples/neo.rb
+++ samples/neo.rb
@@ -6,17 +6,14 @@ class Box < ExampleBase
   end
 
   def init_application
-#    canvas.canvas_renderer.camera.location = Vector3(-6, -6, 0)
-
     root.layout do
       cull_state :back
       skybox(:sky, 500, 500, 500, 'images/skybox/blue')
       node(:spinning) do
-#	color :diffuse
-#	behavior :rotating, Vector3(1, 1, 0.5), 50
+	behavior :rotating, Vector3(1, 1, 0.5), 50
 
 	box(:left_box, 1, 1, 1) do
-#	  behavior :rotating, Vector3(0.5, 1, 1), 50
+	  behavior :rotating, Vector3(0.5, 1, 1), 50
 	  at 0, 0, -3
 	  texture "images/ardor3d_white_256.jpg"
 	end.duplicate_as(:right_box) do
@@ -24,16 +21,7 @@ class Box < ExampleBase
 	end
       end
     end
-
-    init_zbuffer
-
-    root.print_tree
-    puts root.scene_hints.to_s
   end
-
-#  def update_application(r)
-#    puts root.getLocalLastFrustumIntersection
-#  end
 end
 
 Box.start
